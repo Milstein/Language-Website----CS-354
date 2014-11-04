54cd7ee6a593e62454fa861c52914d295aa1d7b6
Milstein <milsonmun@yahoo.com>
2014-11-03 19:14:00 -0700

    added File handling example

---
+++ loops.rb
@@ -0,0 +1,13 @@
+#!/bin/ruby
+## For and for each loop
+##
+## Milson Munakami
+## http://milstein.me
+
+for i in 0..2
+  puts "I am inside the loop number #{i}"
+end
+
+(0..8).each do |j|
+  puts "I am inside the loop number #{j}"
+end
