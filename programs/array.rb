cf13965c1099194b663b85c2896fd7b3f3851406
zhenyuwang0720 <yachibang@gmail.com>
2014-11-07 01:08:01 -0700

    arrays.rb
    demonstrate some functions of array.

---
+++ programs/array.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/ruby
+
+## Demonstrate some basic methods of arrays in ruby.
+
+## Jimmy Wang
+
+shad = ["Milson", "Aaron", "Brent", "Said", "Jimmy"]
+shad.pop
+print shad
+shad.push("Jimmy")
+print shad
+shad.shift
+print shad
+shad.unshift("Milson")
+print shad
