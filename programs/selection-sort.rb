a15b24bb8b3828aa59e7ccba3126ee285bba8968
sbarrero <saidbarrero@u.boisestate.edu>
2014-11-26 23:16:49 -0700

    adding a selection sort program in ruby

---
+++ programs/selection-sort.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/ruby
+#
+## author: Said Barrero
+## note: change numbers below array to see different results of this algorithm
+
+	def selectionSort(array, p, r)
+        largest=0;
+ 
+        if p < r
+			largest = p
+       		for i in p+1..r do
+       			if array[i] >= array[largest]
+            		largest = i
+				end
+       		end
+ 
+      	temp = array[r]
+        array[r] = array[largest]
+      	array[largest] = temp
+ 
+        selectionSort(array,p,r - 1)
+		end
+    end
+
+puts 'This is a simple selection sort example'
+    numbers = [1,5,3,2,6]
+ 
+    p = 0
+    r = numbers.size - 1
+
+selectionSort(numbers, p, r)
+puts numbers
