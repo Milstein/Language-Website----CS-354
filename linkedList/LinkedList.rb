501d8bce1c47e5d1d1f10617327b61f23c9ab1e5
Aaron Russell <russell.aaron94@gmail.com>
2014-12-03 23:55:07 -0700

    Minor formatting changes

--- linkedList/LinkedList.rb
+++ linkedList/LinkedList.rb
@@ -4,66 +4,66 @@ require './Node.rb'
 ## Represents a doubly-linked, null-terminated list
 class LinkedList
 
-	## Initializes the list
-	def initialize
-		@size = 0
-		@head = nil
-		@tail = nil
-	end
-	
-	## Inserts an element at the beginning of the list 
-	def insertFront(element)
-	
-		node = Node.new(element)
-		
-		if @size == 0
-			@head = node
-			@tail = node
-		else
-			@head.setPrev(node)
-			node.setNext(@head)
-			@head = node
-		end
-		
-		@size += 1
-		
-	end
-	
-	## Inserts an element at the end of the list
-	def insertRear(element)
-	
-		node = Node.new(element)
-		
-		if @size == 0
-			@head = node
-			@tail = node
-		else
-			@tail.setNext(node)
-			node.setPrev(@tail)
-			@tail = node
-		end
-		
-		@size += 1
-		
-	end
+    ## Initializes the list
+    def initialize
+        @size = 0
+        @head = nil
+        @tail = nil
+    end
+    
+    ## Inserts an element at the beginning of the list 
+    def insertFront(element)
+    
+        node = Node.new(element)
+        
+        if @size == 0
+            @head = node
+            @tail = node
+        else
+            @head.setPrev(node)
+            node.setNext(@head)
+            @head = node
+        end
+        
+        @size += 1
+        
+    end
+    
+    ## Inserts an element at the end of the list
+    def insertRear(element)
+    
+        node = Node.new(element)
+        
+        if @size == 0
+            @head = node
+            @tail = node
+        else
+            @tail.setNext(node)
+            node.setPrev(@tail)
+            @tail = node
+        end
+        
+        @size += 1
+        
+    end
     
     ## Removes the element at the beginning of the list
     def removeFront()
-       
-       if @size > 1
+    
+        if @size > 1
             retVal = @head.getData
             @head = @head.getNext()
             @head.setPrev(nil)
             @size -= 1
-       elsif @size == 1
-			retVal = @head.getData
+        elsif @size == 1
+            retVal = @head.getData
             clear
-       else
-			retVal = nil
-       end
-       
-       return retVal
-       
+        else
+            retVal = nil
+        end
+        
+        return retVal
+        
     end
     
     ## Removes the element at the end of the list
@@ -75,89 +75,89 @@ class LinkedList
             @tail.setNext(nil)
             @size -= 1
         elsif @size == 1
-			retVal = @tail.getData
+            retVal = @tail.getData
             clear
         else
-			retVal = nil
+            retVal = nil
         end
         
         return retVal
         
     end
-	
-	## Removes the given element from the list 
-	def removeElement(element)
-		
-		if @size == 0
-			return nil
-		end
-		
-		## if the head contains the element
-		if @head.getData == element
-			return removeFront
-		end
-		
-		## if the tail contains the element
-		if @tail.getData == element
-			return removeRear
-		end
-		
-		## try to find the element in the list
-		curr = @head
-		while curr != nil
-			if curr.getData == element
-				curr.getPrev.setNext(curr.getNext)
-				curr.getNext.setPrev(curr.getPrev)
-				@size -= 1
-				return curr.getData
-			else
-				curr = curr.getNext
-			end
-		end
-		
-		## if the element wasn't found, return nil
-		return nil
-		
-	end
-
-	## Searches the list for the given element
-   	def search(element)
-   	
-		curr = @head
-		while curr != nil
-			if curr.getData == element
-				return curr.getData
-			else
-				curr = curr.getNext
-			end
-		end
-		
-		## if the element wasn't found, return nil
-		return nil
-   	
-	end
-	
-	## Clears the list
-	def clear
-		@size = 0
-		@head = nil
-		@tail = nil
-	end
-
-	## Generates a string representation of the list
-	def to_s
-	
-		curr = @head
-		str = ""
-		
-		while curr != nil do
-			str += curr.getData.to_s
-			str += "\n"
-			curr = curr.getNext
-		end
-		
-		return str
-		
-	end
-	
+    
+    ## Removes the given element from the list 
+    def removeElement(element)
+    
+        if @size == 0
+            return nil
+        end
+        
+        ## if the head contains the element
+        if @head.getData == element
+            return removeFront
+        end
+        
+        ## if the tail contains the element
+        if @tail.getData == element
+            return removeRear
+        end
+        
+        ## try to find the element in the list
+        curr = @head
+        while curr != nil
+            if curr.getData == element
+                curr.getPrev.setNext(curr.getNext)
+                curr.getNext.setPrev(curr.getPrev)
+                @size -= 1
+                return curr.getData
+            else
+                curr = curr.getNext
+            end
+        end
+        
+        ## if the element wasn't found, return nil
+        return nil
+        
+    end
+    
+    ## Searches the list for the given element
+    def search(element)
+    
+        curr = @head
+        while curr != nil
+            if curr.getData == element
+                return curr.getData
+            else
+                curr = curr.getNext
+            end
+        end
+        
+        ## if the element wasn't found, return nil
+        return nil
+        
+    end
+    
+    ## Clears the list
+    def clear
+        @size = 0
+        @head = nil
+        @tail = nil
+    end
+    
+    ## Generates a string representation of the list
+    def to_s
+    
+        curr = @head
+        str = ""
+        
+        while curr != nil do
+            str += curr.getData.to_s
+            str += "\n"
+            curr = curr.getNext
+        end
+        
+        return str
+        
+    end
+    
 end
